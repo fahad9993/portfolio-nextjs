088264ad063728abbf3f1bfb9de5c86aff1f9374
Fahad <fahad9993@gmail.com>
2023-08-30 08:09:16 +0600

    Change font to Nunito Sans

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -17,36 +17,43 @@ export default function Sidebar() {
         <Image
             loader={imageLoader}
             src='profilePic.png'
-            width={200}
-            height={200}
+            width={100}
+            height={100}
+            className='rounded-full mx-auto'
             alt='user avatar'
         />
-        <h3>Md. <span>Fahad</span> Rahman</h3>
-        <p>Mechanical Engineer</p>
-        <p><GiTie className='w-6 h-6'/> Download Resume</p>
+        <h3 className='my-4 text-3xl font-medium tracking-wider'>Md. <span>Fahad</span> Rahman</h3>
+        <p className='px-2 py-1 my-3 bg-gray-200 rounded-full'>Mechanical Engineer</p>
+        <Link
+            href='https://fahad9993.github.io/cv-new/assets/pdf/Fahad-cv.pdf'
+            className='px-2 py-1 my-3 bg-gray-200 rounded-full flex justify-center items-center'>
+            <GiTie className='w-6 h-6'/> Download Resume
+        </Link>
         {/* social icons */}
-        <div>
+        <div className='flex justify-around w-9/12 mx-auto my-5 text-green-500 md:w-full'>
             <Link href='https://www.facebook.com/fahad9993'>
-                <AiFillFacebook className='w-8 h-8'/>
+                <AiFillFacebook className='w-8 h-8 cursor-pointer'/>
             </Link>
             <Link href='https://www.github.com/fahad9993'>
-                <AiFillGithub className='w-8 h-8'/>
+                <AiFillGithub className='w-8 h-8 cursor-pointer'/>
             </Link>
             <Link href='https://www.linkedin.com/in/fahad9993'>
-                <AiFillLinkedin className='w-8 h-8'/>
+                <AiFillLinkedin className='w-8 h-8 cursor-pointer'/>
             </Link>
         </div>
         {/* address */}
-        <div>
-            <div>
+        <div
+            className='py-4 my-5 bg-gray-200'
+            style = {{ marginLeft: '-1rem', marginRight: '-1rem' }}>
+            <div className='flex items-center justify-center space-x-2'>
                 <GoLocation className='w-8 h-8'/>
                 <span>Dhaka, Bangladesh</span>
             </div>
-            <p>fahad9993@gmail.com</p>
-            <p>+880-01760979908</p>
+            <p className='my-2'>fahad9993@gmail.com</p>
+            <p className='my-2'>+880-01760979908</p>
             {/* buttons */}
-            <button>Email Me</button>
-            <button>Toggle Theme</button>
+            <button className='bg-gradient-to-r from-green-400 to-blue-400 w-8/12 rounded-full py-2 px-5 text-white my-2'>Email Me</button>
+            <button className='bg-gradient-to-r from-green-400 to-blue-400 w-8/12 rounded-full py-2 px-5 text-white'>Toggle Theme</button>
         </div>
 
     </div>
