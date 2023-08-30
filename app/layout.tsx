6f3b546dc7870c7033fae56bf8d1e81c5bac2203
Fahad <fahad9993@gmail.com>
2023-08-30 12:37:17 +0600

    Final Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Nunito_Sans } from "next/font/google";
+import Sidebar from "@/components/Sidebar";
 
 const nunito_sans = Nunito_Sans({ subsets: ["latin"] });
 
@@ -19,7 +21,15 @@ export default function RootLayout({
       <body
         className={`${nunito_sans.className} bg-gradient-to-r from-green to to-blue-400`}
       >
-        {children}
+        <main className="grid grid-cols-12 gap-6 my-14 px-5 lg:px-48 sm:px-20 md:px-32">
+          <div className="col-span-12 lg:col-span-3 bg-white rounded-2xl p-4 text-center">
+            <Sidebar />
+          </div>
+          <div className="col-span-12 lg:col-span-9 bg-white rounded-2xl flex flex-col">
+            <Navbar />
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,5 @@ import Sidebar from "@/components/Sidebar";
 import { Component } from "react";
 
 export default function Home() {
-  return (
-    <main className="grid grid-cols-12 gap-6 my-14 px-5 lg:px-48 sm:px-20 md:px-32">
-      <div className="col-span-12 lg:col-span-3 bg-white rounded-2xl p-4 text-center">
-        <Sidebar />
-      </div>
-      <div className="col-span-12 lg:col-span-9 bg-white rounded-2xl flex flex-col">
-        <Navbar />
-        New Components
-      </div>
-    </main>
-  );
+  return <div className="p-4">Home page or About Page</div>;
 }
