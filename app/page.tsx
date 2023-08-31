77e33b33019c6a9efc29099d9065150717cb754c
Fahad <fahad9993@gmail.com>
2023-08-31 08:32:07 +0600

    Add ServiceCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
 // https://www.youtube.com/playlist?list=PLQKg8mIgoxKpvIWyxMM-Nn6s_iww0KX53
 // Using the above playlist for creating this portfolio
 import { services } from "@/data";
+import ServiceCard from "@/components/ServiceCard";
 
 export default function Home() {
-  return <div className="p-4">Home page or About Page</div>;
+  return (
+    <div className="p-4">
+      <h5>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged.
+      </h5>
+      <div>
+        <h6>What I offer</h6>
+        <div>
+          {services.map((service) => (
+            <ServiceCard service={service} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
