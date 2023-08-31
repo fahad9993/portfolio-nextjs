fb3fcb3d4fffc61f3fc80547dc559251a4363ebf
Fahad <fahad9993@gmail.com>
2023-08-31 10:40:40 +0600

    Some more service data

--- data.ts
+++ data.ts
@@ -2,6 +2,7 @@ import { RiComputerLine } from "react-icons/ri";
 import { FaServer, FaCode } from "react-icons/fa";
 import { AiOutlineApi } from "react-icons/ai";
 import { SiLatex } from "react-icons/si";
+import { SlLayers } from "react-icons/sl";
 import { IService } from "@/types";
 import styles from "@/components/styles.module.css";
 
@@ -34,6 +35,12 @@ export const services: IService[] = [
     Icon: SiLatex,
     title: "Typesetting",
     about:
-      "Work with <b><span className={styles.latex}>L<sup>A</sup>T<sub>E</sub>X</span></b>",
+      "Work with <b><span className={styles.latex}>L<sup>A</sup>T<sub>E</sub>X</span></b> to create beautifully typesetted documents and can handle complex packages",
+  },
+  {
+    Icon: SlLayers,
+    title: "Design",
+    about:
+      "Work with <b>AutoCAD</b>, <b>Solidworks</b> and <b>Adobe Illustrator</b> to create engineering designs, logos and many more",
   },
 ];
