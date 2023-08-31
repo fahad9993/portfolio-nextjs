e46d7041f3ac99b26129edd0a4fb32a7240a45e9
Fahad <fahad9993@gmail.com>
2023-08-31 08:50:18 +0600

    Service Card end

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -1,5 +1,18 @@
+import { IService } from "@/types";
 import React from "react";
 
-export default function ServiceCard() {
-  return <div>ServiceCard</div>;
+export default function ServiceCard({
+  service: { Icon, title, about },
+}: {
+  service: IService;
+}) {
+  return (
+    <div>
+      <Icon />
+      <div>
+        <h4>{title}</h4>
+        <p>{about}</p>
+      </div>
+    </div>
+  );
 }
