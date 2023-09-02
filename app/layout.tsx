7840f68e9f7d0c48c8aca335d9b74bf5741ffbc7
Fahad <fahad9993@gmail.com>
2023-09-02 06:47:41 +0600

    Add theme provider wrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Nunito_Sans } from "next/font/google";
 import Sidebar from "@/components/Sidebar";
+import { ThemeProvider } from "next-themes";
 
 const nunito_sans = Nunito_Sans({ subsets: ["latin"] });
 
@@ -21,15 +24,17 @@ export default function RootLayout({
       <body
         className={`${nunito_sans.className} bg-gradient-to-r from-green to to-blue-400`}
       >
-        <main className="grid grid-cols-12 gap-6 my-14 px-5 lg:px-48 sm:px-20 md:px-32">
-          <div className="col-span-12 lg:col-span-3 bg-white rounded-2xl p-4 text-center">
-            <Sidebar />
-          </div>
-          <div className="col-span-12 lg:col-span-9 bg-white rounded-2xl flex flex-col overflow-hidden">
-            <Navbar />
-            {children}
-          </div>
-        </main>
+        <ThemeProvider>
+          <main className="grid grid-cols-12 gap-6 my-14 px-5 lg:px-48 sm:px-20 md:px-32">
+            <div className="col-span-12 lg:col-span-3 bg-white rounded-2xl p-4 text-center">
+              <Sidebar />
+            </div>
+            <div className="col-span-12 lg:col-span-9 bg-white rounded-2xl flex flex-col overflow-hidden">
+              <Navbar />
+              {children}
+            </div>
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
