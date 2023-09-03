0c4b2d31c718176eee0bbd6d69145231334b1af9
Fahad <fahad9993@gmail.com>
2023-09-03 17:34:11 +0600

    Add interface for projects

--- types.ts
+++ types.ts
@@ -24,3 +24,13 @@ export interface IEducation {
   about: string;
   timeframe: string;
 }
+
+export interface IProject {
+  title: string;
+  description: string;
+  image_path: string;
+  deployed_url: string;
+  github_url: string;
+  category: string[];
+  key_techs: string[];
+}
