aeac4b3366537170d4de87fce741057dbd175a5a
Fahad <fahad9993@gmail.com>
2023-09-04 10:42:47 +0600

    Add final Project Card

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,12 +4,12 @@ import React from "react";
 
 export default function page() {
   return (
-    <div className="p-4">
+    <div className="px-5 py-2 overflow-auto" style={{ height: "65vh" }}>
       <nav>Navbar</nav>
-      <div className="grid grid-cols-12 gap-4 my-3">
-        {projects.map((project) => (
-          <div className="col-span-12 sm:col-span-6 lg:col-span-4">
-            <ProjectCard project={project} key={project.title} />
+      <div className="grid grid-cols-12 gap-4 my-3 relative">
+        {projects.map((project, i) => (
+          <div className="col-span-12 sm:col-span-6 lg:col-span-4 bg-gray-200 dark:bg-dark-200 rounded-lg">
+            <ProjectCard project={project} key={i} />
           </div>
         ))}
       </div>
--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import { IProject } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { AiFillGithub, AiFillProject } from "react-icons/ai";
 import { MdClose } from "react-icons/md";
 
@@ -18,41 +20,64 @@ export default function ProjectCard({
 }: {
   project: IProject;
 }) {
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
     <div>
       <Image
         src={image_path}
-        width={100}
-        height={100}
+        width={200}
+        height={200}
         alt={title}
         className="cursor-pointer"
+        onClick={() => setShowDetails(true)}
       />
-      <p>{title}</p>
+      <p className="my-2 text-center">{title}</p>
 
-      <div className="grid md:grid-cols-2">
-        <div>
-          <Link href={github_url}>
-            <AiFillGithub />
-            <span>Github</span>
-          </Link>
-          <Link href={deployed_url}>
-            <AiFillProject />
-            <span>Project</span>
-          </Link>
-        </div>
-        <div>
-          <h2>{title}</h2>
-          <h3>{description}</h3>
+      {showDetails && (
+        <div className="grid md:grid-cols-2 absolute top-0 left-0 z-10 w-full h-auto gap-x-12 text-black bg-gray-100 dark:text-white dark:bg-dark-100 p-2">
           <div>
-            {key_techs.map((tech) => (
-              <span key={tech}>{tech}</span>
-            ))}
+            <Image src={image_path} width={400} height={400} alt={title} />
+            <div className="flex justify-center my-4 space-x-3">
+              <Link
+                href={github_url}
+                className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200"
+              >
+                <AiFillGithub />
+                <span>Github</span>
+              </Link>
+              <Link
+                href={deployed_url}
+                className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200"
+              >
+                <AiFillProject />
+                <span>Project</span>
+              </Link>
+            </div>
           </div>
+
+          <div>
+            <h2 className="mb-3 text-xl font-medium md:text-2xl ">{title}</h2>
+            <h3 className="mb-3 font-medium">{description}</h3>
+            <div className="flex flex-wrap mt-5 gap-x-2 text-sm tracking-wider">
+              {key_techs.map((tech, i) => (
+                <span
+                  key={i}
+                  className="px-2 py-1 my-1 bg-gray-200 dark:bg-dark-200 rounded-sm"
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+          </div>
+          <button
+            onClick={() => setShowDetails(false)}
+            className="absolute top-3 right-3 p-1 focus:outline-none bg-gray-200 dark:bg-dark-200"
+          >
+            <MdClose size={30} />
+          </button>
         </div>
-        <button>
-          <MdClose size={30} />
-        </button>
-      </div>
+      )}
     </div>
   );
 }
