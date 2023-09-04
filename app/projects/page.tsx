fe75ce6b80f95ffd1c6f2e6faa9dda1c44a5a656
Fahad <fahad9993@gmail.com>
2023-09-04 09:24:07 +0600

    Add Project Card

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -6,10 +6,10 @@ export default function page() {
   return (
     <div className="p-4">
       <nav>Navbar</nav>
-      <div>
+      <div className="grid grid-cols-12 gap-4 my-3">
         {projects.map((project) => (
-          <div>
-            <ProjectCard project={project} />
+          <div className="col-span-12 sm:col-span-6 lg:col-span-4">
+            <ProjectCard project={project} key={project.title} />
           </div>
         ))}
       </div>
--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,5 +1,9 @@
 import { IProject } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { AiFillGithub, AiFillProject } from "react-icons/ai";
+import { MdClose } from "react-icons/md";
 
 export default function ProjectCard({
   project: {
@@ -14,5 +18,41 @@ export default function ProjectCard({
 }: {
   project: IProject;
 }) {
-  return <div>ProjectCard</div>;
+  return (
+    <div>
+      <Image
+        src={image_path}
+        width={100}
+        height={100}
+        alt={title}
+        className="cursor-pointer"
+      />
+      <p>{title}</p>
+
+      <div className="grid md:grid-cols-2">
+        <div>
+          <Link href={github_url}>
+            <AiFillGithub />
+            <span>Github</span>
+          </Link>
+          <Link href={deployed_url}>
+            <AiFillProject />
+            <span>Project</span>
+          </Link>
+        </div>
+        <div>
+          <h2>{title}</h2>
+          <h3>{description}</h3>
+          <div>
+            {key_techs.map((tech) => (
+              <span key={tech}>{tech}</span>
+            ))}
+          </div>
+        </div>
+        <button>
+          <MdClose size={30} />
+        </button>
+      </div>
+    </div>
+  );
 }
