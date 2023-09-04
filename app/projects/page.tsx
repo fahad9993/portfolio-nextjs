a3e7faa31d5b9d567f3611ceb54f1f602f29f8ed
Fahad <fahad9993@gmail.com>
2023-09-04 12:40:30 +0600

    Fix error to move mapping key to to level div

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -31,8 +31,11 @@ export default function page() {
       </nav>
       <div className="grid grid-cols-12 gap-4 my-3 relative">
         {projects.map((project) => (
-          <div className="col-span-12 sm:col-span-6 lg:col-span-4 bg-gray-200 dark:bg-dark-200 rounded-lg">
-            <ProjectCard project={project} key={project.title} />
+          <div
+            className="col-span-12 sm:col-span-6 lg:col-span-4 bg-gray-200 dark:bg-dark-200 rounded-lg"
+            key={project.title}
+          >
+            <ProjectCard project={project} />
           </div>
         ))}
       </div>
