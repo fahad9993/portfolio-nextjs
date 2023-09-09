ab8a0b3125705b246a3b3cdbffaecb74c99aaf07
Fahad <fahad9993@gmail.com>
2023-09-09 08:02:22 +0600

    Add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import Sidebar from "@/components/Sidebar";
 import { ThemeProvider } from "next-themes";
 import { AnimatePresence } from "framer-motion";
 import Head from "next/head";
+import Footer from "@/components/Footer";
 
 const nunito_sans = Nunito_Sans({ subsets: ["latin"] });
 
@@ -34,7 +35,7 @@ export default function RootLayout({
         className={`${nunito_sans.className} bg-gradient-to-r from-green to-blue-400 dark:from-dark-500 dark:to-dark-700 dark:text-white`}
       >
         <ThemeProvider attribute="class">
-          <main className="grid grid-cols-12 gap-6 my-14 px-5 lg:px-48 sm:px-20 md:px-32">
+          <main className="grid grid-cols-12 gap-6 mt-14 mb-6 px-5 lg:px-48 sm:px-20 md:px-32">
             <div className="col-span-12 lg:col-span-3 bg-white dark:bg-dark-500 rounded-2xl p-4 text-center shadow-custom-light dark:shadow-custom-dark">
               <Sidebar />
             </div>
@@ -43,6 +44,9 @@ export default function RootLayout({
               <AnimatePresence mode="wait">{children}</AnimatePresence>
             </div>
           </main>
+          <footer className="mb-14 px-5 lg:px-48 sm:px-20 md:px-32">
+            <Footer />
+          </footer>
         </ThemeProvider>
       </body>
     </html>
