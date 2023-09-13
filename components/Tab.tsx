b134dfe3376527348564aef893cb6013ec2a7df6
Fahad <fahad9993@gmail.com>
2023-09-13 13:37:36 +0600

    Add hover effect

--- components/Tab.tsx
+++ components/Tab.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
@@ -27,20 +28,23 @@ export default function BasicTabs({
 
   return (
     <div className="border-b border-gray-200 mb-4">
-      <nav className="-mb-px flex">
+      <nav className="flex flex-col sm:flex-row justify-center items-center">
         {tabItems.map((item, index) => (
           <div
             key={index}
             onClick={() => handleTabClick(index)}
-            className={`cursor-pointer py-2 px-4 inline-flex items-center capitalize ${
+            className={`cursor-pointer py-2 px-4 capitalize flex-grow ${
               activeTab === index
                 ? "bg-gradient-to-r from-green to-blue-400 text-white"
-                : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
+                : "border-transparent text-gray-500 hover:bg-gradient-to-r hover:from-green hover:to-blue-600 hover:text-gray-100 transition duration-300 ease-in-out"
             }`}
           >
             {item}
           </div>
         ))}
+        <div className="flex-grow cursor-pointer py-2 px-4 hover:bg-gradient-to-r hover:from-green hover:to-blue-600 hover:text-gray-100 transition duration-300 ease-in-out">
+          <Link href="/resume">Go Back</Link>
+        </div>
       </nav>
     </div>
   );
