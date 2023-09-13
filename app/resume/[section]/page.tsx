be58a152aa7b32692cd2c0b6e9046239fe98a8e2
Fahad <fahad9993@gmail.com>
2023-09-13 14:49:14 +0600

    Dynamic Metadata for section of Resume

--- app/resume/[section]/page.tsx
+++ app/resume/[section]/page.tsx
@@ -1,51 +1,21 @@
-"use client";
+import ResumeSection from "./ResumeSection";
+import { Metadata, ResolvingMetadata } from "next";
 
-import React, { useState } from "react";
-import { motion } from "framer-motion";
-import { routeAnimation } from "@/animations";
-import Tab from "@/components/Tab";
-import { educations, experiences } from "@/data";
-import EduExpCard from "@/components/EduExpCard";
-
-export default function page({ params }: { params: { section: string } }) {
+export async function generateMetadata(
+  { params }: { params: { section: string } },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
   const section = params.section;
-  // State to store the selected tabName
-  const [selectedTab, setSelectedTab] = useState<string | null>(section);
+  // Coverting section name to Capitalize
+  const section_cap = section.charAt(0).toUpperCase() + section.slice(1);
 
-  // Function to handle tab clicks and update selectedTab state
-  const handleTabClick = (tabName: string) => {
-    setSelectedTab(tabName);
+  return {
+    title: `${section_cap} | Resume`,
   };
+}
 
-  return (
-    <motion.div
-      className="px-6 py-2"
-      variants={routeAnimation}
-      initial="initial"
-      animate="animate"
-      exit="exit"
-    >
-      <Tab section={selectedTab || section} onTabClick={handleTabClick} />
-      {/* Render content based on the selected tab */}
-      {selectedTab === "experience" && (
-        <div>
-          {experiences.map((card) => (
-            <div key={card.place}>
-              <EduExpCard card={card} />
-            </div>
-          ))}
-        </div>
-      )}
-
-      {selectedTab === "education" && (
-        <div>
-          {educations.map((card) => (
-            <div key={card.title}>
-              <EduExpCard card={card} />
-            </div>
-          ))}
-        </div>
-      )}
-    </motion.div>
-  );
+export default function Projects({ params }: { params: { section: string } }) {
+  const section = params.section;
+  return <ResumeSection section={section} />;
 }
