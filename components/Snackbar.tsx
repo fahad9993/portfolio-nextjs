a65c5f0d6e869b8d00020b9e4714e0ba1b3b2c91
Fahad <fahad9993@gmail.com>
2023-09-15 11:34:45 +0600

    Modify to use notistack instead of material ui

--- components/Snackbar.tsx
+++ components/Snackbar.tsx
@@ -1,47 +1,31 @@
 import * as React from "react";
-import Stack from "@mui/material/Stack";
-import Snackbar from "@mui/material/Snackbar";
-import MuiAlert, { AlertProps } from "@mui/material/Alert";
+import { useSnackbar } from "notistack";
 import { Severity } from "@/types";
 
-const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
-  props,
-  ref
-) {
-  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-});
-
 type Props = {
-  message: string[];
-  open: boolean;
-  setOpen: (value: boolean) => void;
+  errors: string[];
   severity: Severity;
 };
 
-export default function SnackBar({ message, open, setOpen, severity }: Props) {
-  const handleClose = (
-    event?: React.SyntheticEvent | Event,
-    reason?: string
-  ) => {
-    if (reason === "clickaway") {
-      return;
+const SnackBar: React.FC<Props> = ({ errors, severity }) => {
+  const { enqueueSnackbar } = useSnackbar();
+
+  React.useEffect(() => {
+    if (errors.length > 0) {
+      errors.forEach((error) => {
+        enqueueSnackbar(error, {
+          variant: severity,
+          autoHideDuration: 5000,
+          anchorOrigin: {
+            vertical: "top",
+            horizontal: "center",
+          },
+        });
+      });
     }
+  }, [errors, enqueueSnackbar, severity]);
 
-    setOpen(false);
-  };
+  return null;
+};
 
-  return (
-    <Stack spacing={2} sx={{ width: "100%" }}>
-      <Snackbar
-        open={open}
-        autoHideDuration={5000}
-        onClose={handleClose}
-        anchorOrigin={{ vertical: "top", horizontal: "center" }}
-      >
-        <Alert onClose={handleClose} severity={severity} sx={{ width: "100%" }}>
-          {message}
-        </Alert>
-      </Snackbar>
-    </Stack>
-  );
-}
+export default SnackBar;
