71ea9942c736b4cb7a751e6e916079ea56f60c94
Fahad <fahad9993@gmail.com>
2023-09-22 09:34:26 +0600

    Deactivate automatic email reply

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -13,27 +13,27 @@ export async function POST(req: NextRequest) {
 
     // Send acknowledgment email
     // Create a Nodemailer transporter using the SMTP settings
-    const transporter = nodemailer.createTransport({
-      host: "smtp.office365.com",
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
+    // const transporter = nodemailer.createTransport({
+    //   host: "smtp.office365.com",
+    //   port: 587,
+    //   secure: false, // true for 465, false for other ports
+    //   auth: {
+    //     user: process.env.EMAIL_USERNAME,
+    //     pass: process.env.EMAIL_PASSWORD,
+    //   },
+    // });
 
-    const mailOptions = {
-      from: process.env.EMAIL_USERNAME,
-      to: email, // Use the user's email from the contact form
-      subject: "Thank you for contacting me!",
-      html: `<p>Hello ${fullname},</p>
-      <p>Thank you for contacting me. I will get back to you soon.</p>
-      <p>Best Regards,</p>
-      <p style="color: blue;">Md. Fahad Rahman</p>`,
-    };
+    // const mailOptions = {
+    //   from: process.env.EMAIL_USERNAME,
+    //   to: email, // Use the user's email from the contact form
+    //   subject: "Thank you for contacting me!",
+    //   html: `<p>Hello ${fullname},</p>
+    //   <p>Thank you for contacting me. I will get back to you soon.</p>
+    //   <p>Best Regards,</p>
+    //   <p style="color: blue;">Md. Fahad Rahman</p>`,
+    // };
 
-    await transporter.sendMail(mailOptions);
+    // await transporter.sendMail(mailOptions);
 
     return NextResponse.json({
       msg: ["Message sent successfully"],
