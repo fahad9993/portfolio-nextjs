aea383111787040516965a9ecad15a92b1490a0c
Fahad <fahad9993@gmail.com>
2023-09-22 07:44:42 +0600

    Add options for automatic email reply

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/app/lib/mongodb";
 import Contact from "@/app/models/contact";
 import mongoose from "mongoose";
+import nodemailer from "nodemailer";
 
 export async function POST(req: NextRequest) {
   const { fullname, email, message } = await req.json();
@@ -9,6 +10,30 @@ export async function POST(req: NextRequest) {
   try {
     await connectDB();
     await Contact.create({ fullname, email, message });
+
+    // Send acknowledgment email
+    // Create a Nodemailer transporter using the SMTP settings
+    const transporter = nodemailer.createTransport({
+      host: "smtp.office365.com",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USERNAME,
+      to: email, // Use the user's email from the contact form
+      subject: "Thank you for contacting me!",
+      html: `<p>Hello ${fullname},</p>
+      <p>Thank you for contacting me. I will get back to you soon.</p>
+      <p>Best Regards,</p>
+      <p style="color: blue;">Md. Fahad Rahman</p>`,
+    };
+
+    await transporter.sendMail(mailOptions);
 
     return NextResponse.json({
       msg: ["Message sent successfully"],
@@ -23,6 +48,7 @@ export async function POST(req: NextRequest) {
       console.log(errorList);
       return NextResponse.json({ msg: errorList });
     } else {
+      // console.error("Unable to send message:", error);
       return NextResponse.json({ msg: ["Unable to send message."] });
     }
   }
