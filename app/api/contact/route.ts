e86cf2262786960fdb6f76d6873d2613cc162483
Fahad <fahad9993@gmail.com>
2023-09-23 19:04:34 +0600

    Add transporter close block

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -4,46 +4,47 @@ import Contact from "@/app/models/contact";
 import mongoose from "mongoose";
 import { Resend } from "resend";
 import EmailTemplate from "@/components/EmailTemplate";
+import nodemailer from "nodemailer";
 
 export async function POST(req: NextRequest) {
   const { fullname, email, message } = await req.json();
   const resend = new Resend(process.env.RESEND_API_KEY);
 
+  // Create a Nodemailer transporter using the SMTP settings
+  const transporter = nodemailer.createTransport({
+    host: "smtp.office365.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USERNAME,
+    to: email, // Use the user's email from the contact form
+    subject: "Thank you for contacting me!",
+    html: `<p>Hello ${fullname},</p>
+    <p>Thank you for contacting me. I will get back to you soon.</p>
+    <p>Best Regards,</p>
+    <p style="color: blue;">Md. Fahad Rahman</p>`,
+  };
+
   try {
     await connectDB();
     await Contact.create({ fullname, email, message });
 
-    // Send acknowledgment email==
-    // Create a Nodemailer transporter using the SMTP settings
-    // const transporter = nodemailer.createTransport({
-    //   host: "smtp.office365.com",
-    //   port: 587,
-    //   secure: false, // true for 465, false for other ports
-    //   auth: {
-    //     user: process.env.EMAIL_USERNAME,
-    //     pass: process.env.EMAIL_PASSWORD,
-    //   },
-    // });
-
-    // const mailOptions = {
-    //   from: process.env.EMAIL_USERNAME,
-    //   to: email, // Use the user's email from the contact form
-    //   subject: "Thank you for contacting me!",
-    //   html: `<p>Hello ${fullname},</p>
-    //   <p>Thank you for contacting me. I will get back to you soon.</p>
-    //   <p>Best Regards,</p>
-    //   <p style="color: blue;">Md. Fahad Rahman</p>`,
-    // };
-
-    // await transporter.sendMail(mailOptions);
+    // Send acknowledgment email
+    await transporter.sendMail(mailOptions);
     // await wrapedSendMail(mailOptions);
 
-    await resend.emails.send({
-      from: "Md. Fahad Rahman | Portfolio <onboarding@resend.dev>",
-      to: [email],
-      subject: "Thank you for contacting me!",
-      react: EmailTemplate({ fullname: fullname }),
-    });
+    // await resend.emails.send({
+    //   from: "Md. Fahad Rahman | Portfolio <onboarding@resend.dev>",
+    //   to: [email],
+    //   subject: "Thank you for contacting me!",
+    //   react: EmailTemplate({ fullname: fullname }),
+    // });
 
     return NextResponse.json({
       msg: ["Message sent successfully"],
@@ -61,5 +62,8 @@ export async function POST(req: NextRequest) {
       // console.error("Unable to send message:", error);
       return NextResponse.json({ msg: ["Unable to send message."] });
     }
+  } finally {
+    // Close the transporter
+    transporter.close();
   }
 }
